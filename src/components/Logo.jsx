8bf91f7388176ba951562203bd96cfad6044754b
Vishakha More <vishakhamore252@gmail.com>
2025-08-19 00:36:43 +0530

    design changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import Profile from './Profile'
 
 const Logo = () => {
   return (
-    <div className='md:w-1/4 w-full flex md:flex-col flex-row justify-between'>
+    <div className='md:w-64 w-full flex md:flex-col flex-row justify-between'>
       <div className='text-2xl p-4'>
           <img src="src\assets\spotify-logo.png" alt="Spotify Logo" />
       </div>
